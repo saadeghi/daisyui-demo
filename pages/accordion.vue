fd2c2902707f694236ebe2047fd35c12194a8698
Pouya <pouya.saadeghi@gmail.com>
2020-11-21 22:01:41 +0330

    accordion

--- pages/accordion.vue
+++ pages/accordion.vue
@@ -2,51 +2,125 @@
   <div>
     <Wrapper title="accordion" classes="flex flex-col space-y-2">
       <Accordion>
-        <li>
-          <input type="checkbox" id="chck1" />
-          <label class="text-xl font-medium accordion-title" for="chck1">
-            Aperiam iure expedita non fugit?
-          </label>
-          <div class="accordion-body">
-            <p>
-              Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
-              est nostrum maiores aut. Voluptas nisi totam eius architecto eius
-              mollitia quos assumenda non. Iusto rerum doloribus. Architecto
-              dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
-              dolores iure ut debitis ad.
-            </p>
-          </div>
-        </li>
-        <li>
-          <input type="checkbox" id="chck2" />
-          <label class="text-xl font-medium accordion-title" for="chck2">
-            Aperiam iure expedita non fugit?
-          </label>
-          <div class="accordion-body">
-            <p>
-              Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
-              est nostrum maiores aut. Voluptas nisi totam eius architecto eius
-              mollitia quos assumenda non. Iusto rerum doloribus. Architecto
-              dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
-              dolores iure ut debitis ad.
-            </p>
-          </div>
-        </li>
-        <li>
-          <input type="checkbox" id="chck3" />
-          <label class="text-xl font-medium accordion-title" for="chck3">
-            Aperiam iure expedita non fugit?
-          </label>
-          <div class="accordion-body">
-            <p>
-              Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
-              est nostrum maiores aut. Voluptas nisi totam eius architecto eius
-              mollitia quos assumenda non. Iusto rerum doloribus. Architecto
-              dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
-              dolores iure ut debitis ad.
-            </p>
-          </div>
-        </li>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+      </Accordion>
+    </Wrapper>
+    <Wrapper title="accordion with arrow" classes="flex flex-col space-y-2">
+      <Accordion class="arrow">
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+      </Accordion>
+    </Wrapper>
+    <Wrapper
+      title="accordion with plus/minus"
+      classes="flex flex-col space-y-2"
+    >
+      <Accordion class="plus">
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
+        <AccordionItem
+          classes="text-xl font-medium accordion-title"
+          title="Aperiam iure expedita non fugit?"
+        >
+          <p>
+            Aperiam iure expedita non fugit. Voluptatem est repellat a aut sit
+            est nostrum maiores aut. Voluptas nisi totam eius architecto eius
+            mollitia quos assumenda non. Iusto rerum doloribus. Architecto
+            dolorum explicabo omnis quidem. Quibusdam ipsam beatae soluta et
+            dolores iure ut debitis ad.
+          </p>
+        </AccordionItem>
       </Accordion>
     </Wrapper>
   </div>
