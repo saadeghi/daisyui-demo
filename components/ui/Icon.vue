8eec9e0ecc1d1b09548f3d1f1008f06d601e26f9
Pouya <pouya.saadeghi@gmail.com>
2020-12-09 04:28:14 +0330

    add icon

--- components/ui/Icon.vue
+++ components/ui/Icon.vue
@@ -10,6 +10,7 @@
     <path v-if="glyph === 'code'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
     <path v-if="glyph === 'eye'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
       <path v-if="glyph === 'eye'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+    <path v-if="glyph === 'folder'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
   </svg>
 </template>
 
