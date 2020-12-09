d34a78b92d9b5dcfe48482db555c30d3b651556a
Pouya <pouya.saadeghi@gmail.com>
2020-12-09 04:47:44 +0330

    add homepage content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    ༼ つ ◕_◕ ༽つ
+    <div class="prose text-content-800">
+      <h1>DaisyUI</h1>
+      <p>
+        Unstyled (and styled) UI component library for Tailwind CSS users.
+      </p>
+    </div>
+    <div class="grid grid-cols-3 gap-6 mt-10">
+      <Card class="text-center shadow-2xl side bg-secondary text-content-accent">
+        <div class="card-body">
+          <div class="flex justify-center">
+            <Icon glyph="info" class="inline-block w-8 mb-4 stroke-current" />
+          </div>
+          <p>Read full document on github</p>
+          <div class="justify-center space-x-2 card-actions">
+            <a class="btn active secondary" href="https://github.com/saadeghi/daisyui" target="_blank">
+              Github
+              <Icon glyph="arrow" class="inline-block w-6 ml-2 stroke-current" />
+            </a>
+          </div>
+        </div>
+      </Card>
+      <Card class="text-center shadow-2xl side bg-accent text-content-accent">
+        <div class="card-body">
+          <div class="flex justify-center">
+            <Icon glyph="code" class="inline-block w-8 mb-4 stroke-current" />
+          </div>
+          <p>Use NPM package</p>
+          <div class="justify-center space-x-2 card-actions">
+            <a class="btn active accent" href="https://www.npmjs.com/package/daisyui" target="_blank">
+              NPM
+              <Icon glyph="arrow" class="inline-block w-6 ml-2 stroke-current" />
+            </a>
+          </div>
+        </div>
+      </Card>
+      <Card class="text-center shadow-2xl side bg-primary text-content-accent">
+        <div class="card-body">
+          <div class="flex justify-center">
+            <Icon glyph="eye" class="inline-block w-8 mb-4 stroke-current" />
+          </div>
+          <p>Try it online</p>
+          <div class="justify-center space-x-2 card-actions">
+            <a class="btn active primary" href="https://play.tailwindcss.com/fF3zRoCJSr" target="_blank">
+              Tailwind Play
+              <Icon glyph="arrow" class="inline-block w-6 ml-2 stroke-current" />
+            </a>
+          </div>
+        </div>
+      </Card>
+    </div>
   </div>
 </template>
 
