c0d9361173d254ab4c545fd8ee4664e499c71087
Pouya <pouya.saadeghi@gmail.com>
2020-12-20 16:30:58 +0330

    add checkbox

---
+++ pages/components/input-checkbox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Wrapper title="input" classes="flex flex-col space-y-2 w-48">
+      <div class="p-6 card w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Username</span>
+            <Checkbox class="" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="input primary" classes="flex flex-col space-y-2 w-48">
+      <div class="p-6 card w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Username</span>
+            <Checkbox classes="checkbox-primary" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="input secondary" classes="flex flex-col space-y-2 w-48">
+      <div class="p-6 card w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Username</span>
+            <Checkbox classes="checkbox-secondary" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="input accent" classes="flex flex-col space-y-2 w-48">
+      <div class="p-6 card w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Username</span>
+            <Checkbox classes="checkbox-accent" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+  </div>
+</template>
