9f0fe474e8edbae03b764123b58074d8006a1aaa
Pouya <pouya.saadeghi@gmail.com>
2020-12-22 00:08:54 +0330

    responsive improvements

--- pages/demos/style-guide.vue
+++ pages/demos/style-guide.vue
@@ -16,96 +16,96 @@
     </div>
 
     <div class="pt-32 pb-12">
-      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Typography</div>
+      <div class="inline-block text-2xl font-bold border-b-8 lg:text-7xl text-content-900 border-primary">Typography</div>
     </div>
 
-    <div class="flex space-x-6">
+    <div class="flex flex-col lg:space-x-6 lg:flex-row">
       <div class="space-y-2 font-bold text-content-900">
-        <div class="text-9xl">AaBbCc</div>
-        <div class="text-8xl">AaBbCc</div>
-        <div class="text-7xl">AaBbCc</div>
-        <div class="text-6xl">AaBbCc</div>
-        <div class="text-5xl">AaBbCc</div>
-        <div class="text-4xl">AaBbCc</div>
-        <div class="text-3xl">AaBbCc</div>
-        <div class="text-2xl">AaBbCc</div>
-        <div class="text-xl">AaBbCc</div>
-        <div class="text-lg">AaBbCc</div>
-        <div class="text-base">AaBbCc</div>
-        <div class="text-sm">AaBbCc</div>
-        <div class="text-xs">AaBbCc</div>
+        <div class="text-9xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-8xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-7xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-6xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-5xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-4xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-3xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-2xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-lg">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-base">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-sm">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-xs">AaBb<span class="hidden lg:inline-block">Cc</span></div>
       </div>
       <div class="space-y-2 text-content-900">
-        <div class="text-9xl">AaBbCc</div>
-        <div class="text-8xl">AaBbCc</div>
-        <div class="text-7xl">AaBbCc</div>
-        <div class="text-6xl">AaBbCc</div>
-        <div class="text-5xl">AaBbCc</div>
-        <div class="text-4xl">AaBbCc</div>
-        <div class="text-3xl">AaBbCc</div>
-        <div class="text-2xl">AaBbCc</div>
-        <div class="text-xl">AaBbCc</div>
-        <div class="text-lg">AaBbCc</div>
-        <div class="text-base">AaBbCc</div>
-        <div class="text-sm">AaBbCc</div>
-        <div class="text-xs">AaBbCc</div>
+        <div class="text-9xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-8xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-7xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-6xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-5xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-4xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-3xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-2xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-xl">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-lg">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-base">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-sm">AaBb<span class="hidden lg:inline-block">Cc</span></div>
+        <div class="text-xs">AaBb<span class="hidden lg:inline-block">Cc</span></div>
       </div>
     </div>
 
     <div class="pt-32 pb-12">
-      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Brand Colors</div>
+      <div class="inline-block text-2xl font-bold border-b-8 lg:text-7xl text-content-900 border-primary">Brand Colors</div>
     </div>
 
-    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+    <div class="grid grid-cols-1 gap-6 mt-10 text-xs font-semibold capitalize md:grid-cols-3 lg:grid-cols-4 text-content-400">
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-primary-lighten"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-primary-lighten"></div>
         <div class="py-4">primary lighten</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-primary"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-primary"></div>
         <div class="py-4">primary</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-primary-darken"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-primary-darken"></div>
         <div class="py-4">primary darken</div>
       </div>
     </div>
 
-    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+    <div class="grid grid-cols-1 gap-6 mt-10 text-xs font-semibold capitalize md:grid-cols-3 lg:grid-cols-4 text-content-400">
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary-lighten"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-secondary-lighten"></div>
         <div class="py-4">secondary lighten</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-secondary"></div>
         <div class="py-4">secondary</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary-darken"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-secondary-darken"></div>
         <div class="py-4">secondary darken</div>
       </div>
     </div>
 
-    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+    <div class="grid grid-cols-1 gap-6 mt-10 text-xs font-semibold capitalize md:grid-cols-3 lg:grid-cols-4 text-content-400">
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-accent-lighten"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-accent-lighten"></div>
         <div class="py-4">accent lighten</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-accent"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-accent"></div>
         <div class="py-4">accent</div>
       </div>
       <div>
-        <div class="shadow-lg w-52 h-52 rounded-box bg-accent-darken"></div>
+        <div class="h-16 shadow-lg w-fill lg:w-52 lg:h-52 rounded-box bg-accent-darken"></div>
         <div class="py-4">accent darken</div>
       </div>
     </div>
 
     <div class="pt-32 pb-12">
-      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Content Colors</div>
+      <div class="inline-block text-2xl font-bold border-b-8 lg:text-7xl text-content-900 border-primary">Content Colors</div>
     </div>
 
-    <div class="grid grid-cols-5 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+    <div class="grid grid-cols-2 gap-6 mt-10 text-xs font-semibold capitalize lg:grid-cols-5 text-content-400">
       <div>
         <div class="w-32 h-32 shadow-lg rounded-box bg-default"></div>
         <div class="py-4">default</div>
@@ -149,10 +149,10 @@
     </div>
 
     <div class="pt-32 pb-12">
-      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">State Colors</div>
+      <div class="inline-block text-2xl font-bold border-b-8 lg:text-7xl text-content-900 border-primary">State Colors</div>
     </div>
 
-    <div class="grid grid-cols-5 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+    <div class="grid grid-cols-2 gap-6 mt-10 text-xs font-semibold capitalize lg:grid-cols-5 text-content-400">
       <div>
         <div class="w-32 h-32 shadow-lg rounded-box bg-info"></div>
         <div class="py-4">info</div>
