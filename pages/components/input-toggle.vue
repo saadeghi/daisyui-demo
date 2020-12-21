d14c529db2ef7f296153ec8b9b9e451205e62712
Pouya <pouya.saadeghi@gmail.com>
2020-12-21 20:01:39 +0330

    add toggle component

---
+++ pages/components/input-toggle.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Wrapper title="toggle" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Remember me</span>
+            <Toggle class="" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="toggle primary" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Remember me</span>
+            <Toggle classes="toggle-primary" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="toggle secondary" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Remember me</span>
+            <Toggle classes="toggle-secondary" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+    <Wrapper title="toggle accent" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
+        <FormControl>
+          <label class="cursor-pointer label">
+            <span class="label-text">Remember me</span>
+            <Toggle classes="toggle-accent" />
+          </label>
+        </FormControl>
+      </div>
+    </Wrapper>
+  </div>
+</template>
