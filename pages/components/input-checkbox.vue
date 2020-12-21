da126e853a86eb89b520a67bebbdf7075d211830
Pouya <pouya.saadeghi@gmail.com>
2020-12-21 20:01:18 +0330

    improve checkbox

--- pages/components/input-checkbox.vue
+++ pages/components/input-checkbox.vue
@@ -1,40 +1,40 @@
 <template>
   <div>
-    <Wrapper title="input" classes="flex flex-col space-y-2 w-48">
-      <div class="p-6 card w-92">
+    <Wrapper title="input" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
         <FormControl>
           <label class="cursor-pointer label">
-            <span class="label-text">Username</span>
+            <span class="label-text">Remember me</span>
             <Checkbox class="" />
           </label>
         </FormControl>
       </div>
     </Wrapper>
-    <Wrapper title="input primary" classes="flex flex-col space-y-2 w-48">
-      <div class="p-6 card w-92">
+    <Wrapper title="input primary" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
         <FormControl>
           <label class="cursor-pointer label">
-            <span class="label-text">Username</span>
+            <span class="label-text">Remember me</span>
             <Checkbox classes="checkbox-primary" />
           </label>
         </FormControl>
       </div>
     </Wrapper>
-    <Wrapper title="input secondary" classes="flex flex-col space-y-2 w-48">
-      <div class="p-6 card w-92">
+    <Wrapper title="input secondary" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
         <FormControl>
           <label class="cursor-pointer label">
-            <span class="label-text">Username</span>
+            <span class="label-text">Remember me</span>
             <Checkbox classes="checkbox-secondary" />
           </label>
         </FormControl>
       </div>
     </Wrapper>
-    <Wrapper title="input accent" classes="flex flex-col space-y-2 w-48">
-      <div class="p-6 card w-92">
+    <Wrapper title="input accent" classes="flex flex-col space-y-2 w-60">
+      <div class="p-6 card bordered w-92">
         <FormControl>
           <label class="cursor-pointer label">
-            <span class="label-text">Username</span>
+            <span class="label-text">Remember me</span>
             <Checkbox classes="checkbox-accent" />
           </label>
         </FormControl>
