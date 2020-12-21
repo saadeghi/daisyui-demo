3eb22255d4c23df754d37073b64f6440154a4fec
Pouya <pouya.saadeghi@gmail.com>
2020-12-21 16:56:56 +0330

    add style guide demo page

---
+++ pages/demos/style-guide.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+
+    <div class="items-end justify-start h-96 hero bg-primary rounded-box">
+      <div class="hero-content">
+        <div class="py-6 hero-text">
+          <div class="py-2 font-bold text-9xl text-content-primary">
+            Aa
+          </div>
+          <div class="py-2 text-5xl font-bold text-content-primary">
+            Style Guide Demo
+          </div>
+          <p class="text-content-primary">Omnis quo eveniet veniam quis odit.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="pt-32 pb-12">
+      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Typography</div>
+    </div>
+
+    <div class="flex space-x-6">
+      <div class="space-y-2 font-bold text-content-900">
+        <div class="text-9xl">AaBbCc</div>
+        <div class="text-8xl">AaBbCc</div>
+        <div class="text-7xl">AaBbCc</div>
+        <div class="text-6xl">AaBbCc</div>
+        <div class="text-5xl">AaBbCc</div>
+        <div class="text-4xl">AaBbCc</div>
+        <div class="text-3xl">AaBbCc</div>
+        <div class="text-2xl">AaBbCc</div>
+        <div class="text-xl">AaBbCc</div>
+        <div class="text-lg">AaBbCc</div>
+        <div class="text-base">AaBbCc</div>
+        <div class="text-sm">AaBbCc</div>
+        <div class="text-xs">AaBbCc</div>
+      </div>
+      <div class="space-y-2 text-content-900">
+        <div class="text-9xl">AaBbCc</div>
+        <div class="text-8xl">AaBbCc</div>
+        <div class="text-7xl">AaBbCc</div>
+        <div class="text-6xl">AaBbCc</div>
+        <div class="text-5xl">AaBbCc</div>
+        <div class="text-4xl">AaBbCc</div>
+        <div class="text-3xl">AaBbCc</div>
+        <div class="text-2xl">AaBbCc</div>
+        <div class="text-xl">AaBbCc</div>
+        <div class="text-lg">AaBbCc</div>
+        <div class="text-base">AaBbCc</div>
+        <div class="text-sm">AaBbCc</div>
+        <div class="text-xs">AaBbCc</div>
+      </div>
+    </div>
+
+    <div class="pt-32 pb-12">
+      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Brand Colors</div>
+    </div>
+
+    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-primary-lighten"></div>
+        <div class="py-4">primary lighten</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-primary"></div>
+        <div class="py-4">primary</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-primary-darken"></div>
+        <div class="py-4">primary darken</div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary-lighten"></div>
+        <div class="py-4">secondary lighten</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary"></div>
+        <div class="py-4">secondary</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-secondary-darken"></div>
+        <div class="py-4">secondary darken</div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-4 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-accent-lighten"></div>
+        <div class="py-4">accent lighten</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-accent"></div>
+        <div class="py-4">accent</div>
+      </div>
+      <div>
+        <div class="shadow-lg w-52 h-52 rounded-box bg-accent-darken"></div>
+        <div class="py-4">accent darken</div>
+      </div>
+    </div>
+
+    <div class="pt-32 pb-12">
+      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">Content Colors</div>
+    </div>
+
+    <div class="grid grid-cols-5 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-default"></div>
+        <div class="py-4">default</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-100"></div>
+        <div class="py-4">100</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-200"></div>
+        <div class="py-4">200</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-300"></div>
+        <div class="py-4">300</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-400"></div>
+        <div class="py-4">400</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-500"></div>
+        <div class="py-4">500</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-600"></div>
+        <div class="py-4">600</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-700"></div>
+        <div class="py-4">700</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-800"></div>
+        <div class="py-4">800</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-content-900"></div>
+        <div class="py-4">900</div>
+      </div>
+    </div>
+
+    <div class="pt-32 pb-12">
+      <div class="inline-block font-bold border-b-8 text-7xl text-content-900 border-primary">State Colors</div>
+    </div>
+
+    <div class="grid grid-cols-5 gap-6 mt-10 text-xs font-semibold capitalize text-content-400">
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-info"></div>
+        <div class="py-4">info</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-success"></div>
+        <div class="py-4">success</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-warning"></div>
+        <div class="py-4">warning</div>
+      </div>
+      <div>
+        <div class="w-32 h-32 shadow-lg rounded-box bg-error"></div>
+        <div class="py-4">error</div>
+      </div>
+    </div>
+
+
+
+
+  </div>
+</template>
