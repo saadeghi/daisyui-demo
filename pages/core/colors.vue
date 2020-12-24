1d20ceb56a4647cd21c37e9d71cec8f393bdd6db
Pouya <pouya.saadeghi@gmail.com>
2020-12-24 23:35:55 +0330

    color editor

--- pages/core/colors.vue
+++ pages/core/colors.vue
@@ -35,302 +35,67 @@
 
 
 
-    color generator
-    <div class="grid grid-cols-12 gap-4">
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['p1']['hex']"
-          v-on:change="hexToHsl('p1')"
-        >
-        <div class="text-xs ">
-          primary-lighten
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['p2']['hex']"
-          v-on:change="hexToHsl('p2')"
-        >
-        <div class="text-xs ">
-          primary
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['p3']['hex']"
-          v-on:change="hexToHsl('p3')"
-        >
-        <div class="text-xs ">
-          primary-darken
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['cp']['hex']"
-          v-on:change="hexToHsl('cp')"
-        >
-        <div class="text-xs ">
-          content-primary
-        </div>
-      </div>
+color generator
+<div class="grid grid-cols-2">
 
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['s1']['hex']"
-          v-on:change="hexToHsl('s1')"
-        >
-        <div class="text-xs ">
-          secondary-lighten
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['s2']['hex']"
-          v-on:change="hexToHsl('s2')"
-        >
-        <div class="text-xs ">
-          secondary
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['s3']['hex']"
-          v-on:change="hexToHsl('s3')"
-        >
-        <div class="text-xs ">
-          secondary-darken
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['cs']['hex']"
-          v-on:change="hexToHsl('cs')"
-        >
-        <div class="text-xs ">
-          content-secondary
-        </div>
-      </div>
+<div class="prose">
+<pre><code>:root {
+  --d: {{ colorValues['d']['hsl'] }};
+  --p1: {{ colorValues['p1']['hsl'] }};
+  --p2: {{ colorValues['p2']['hsl'] }};
+  --p3: {{ colorValues['p3']['hsl'] }};
+  --s1: {{ colorValues['s1']['hsl'] }};
+  --s2: {{ colorValues['s2']['hsl'] }};
+  --s3: {{ colorValues['s3']['hsl'] }};
+  --a1: {{ colorValues['a1']['hsl'] }};
+  --a2: {{ colorValues['a2']['hsl'] }};
+  --a3: {{ colorValues['a3']['hsl'] }};
+  --c1: {{ colorValues['c1']['hsl'] }};
+  --c2: {{ colorValues['c2']['hsl'] }};
+  --c3: {{ colorValues['c3']['hsl'] }};
+  --c4: {{ colorValues['c4']['hsl'] }};
+  --c5: {{ colorValues['c5']['hsl'] }};
+  --c6: {{ colorValues['c6']['hsl'] }};
+  --c7: {{ colorValues['c7']['hsl'] }};
+  --c8: {{ colorValues['c8']['hsl'] }};
+  --c9: {{ colorValues['c9']['hsl'] }};
+  --cp: {{ colorValues['cp']['hsl'] }};
+  --cs: {{ colorValues['cs']['hsl'] }};
+  --ca: {{ colorValues['ca']['hsl'] }};
+  --in: {{ colorValues['in']['hsl'] }};
+  --su: {{ colorValues['su']['hsl'] }};
+  --wa: {{ colorValues['wa']['hsl'] }};
+  --er: {{ colorValues['er']['hsl'] }};
+}</code></pre>
+</div>
 
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['a1']['hex']"
-          v-on:change="hexToHsl('a1')"
-        >
-        <div class="text-xs ">
-          accent-lighten
-        </div>
+  <div class="">
+    <div class="flex items-center w-full space-y-3" v-for="(item, index) in colorValues">
+      <div class="flex items-center px-2 text-sm w-44">
+        {{ item.name }}
       </div>
-      <div class="flex flex-col w-full">
+      <div class="grid">
         <input
           type="color"
-          class="block w-full h-14"
-          v-model="colorValues['a2']['hex']"
-          v-on:change="hexToHsl('a2')"
+          class="z-10 w-10 h-10 col-start-1 row-start-1 mt-1 ml-3 outline-none flex-0"
+          v-model="colorValues[index]['hex']"
+          v-on:change="hexToHsl(index)"
         >
-        <div class="text-xs ">
-          accent
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
         <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['a3']['hex']"
-          v-on:change="hexToHsl('a3')"
+          type="text"
+          class="col-start-1 row-start-1 pl-16 ml-2 w-44 input-text bordered"
+          v-model="item.hex"
+          v-on:change="hexToHsl(index)"
         >
-        <div class="text-xs ">
-          accent-darken
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['ca']['hex']"
-          v-on:change="hexToHsl('ca')"
-        >
-        <div class="text-xs ">
-          content-accent
-        </div>
-      </div>
-
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['d']['hex']"
-          v-on:change="hexToHsl('d')"
-        >
-        <div class="text-xs ">
-          default
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c1']['hex']"
-          v-on:change="hexToHsl('c1')"
-        >
-        <div class="text-xs ">
-          content-100
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c2']['hex']"
-          v-on:change="hexToHsl('c2')"
-        >
-        <div class="text-xs ">
-          content-200
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c3']['hex']"
-          v-on:change="hexToHsl('c3')"
-        >
-        <div class="text-xs ">
-          content-300
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c4']['hex']"
-          v-on:change="hexToHsl('c4')"
-        >
-        <div class="text-xs ">
-          content-400
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c5']['hex']"
-          v-on:change="hexToHsl('c5')"
-        >
-        <div class="text-xs ">
-          content-500
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c6']['hex']"
-          v-on:change="hexToHsl('c6')"
-        >
-        <div class="text-xs ">
-          content-600
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c7']['hex']"
-          v-on:change="hexToHsl('c7')"
-        >
-        <div class="text-xs ">
-          content-700
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c8']['hex']"
-          v-on:change="hexToHsl('c8')"
-        >
-        <div class="text-xs ">
-          content-800
-        </div>
-      </div>
-      <div class="flex flex-col w-full">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['c9']['hex']"
-          v-on:change="hexToHsl('c9')"
-        >
-        <div class="text-xs ">
-          content-900
-        </div>
-      </div>
-
-      <div class="flex flex-col w-full col-span-3">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['in']['hex']"
-          v-on:change="hexToHsl('in')"
-        >
-        <div class="text-xs ">
-          info
-        </div>
-      </div>
-      <div class="flex flex-col w-full col-span-3">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['su']['hex']"
-          v-on:change="hexToHsl('su')"
-        >
-        <div class="text-xs ">
-          success
-        </div>
-      </div>
-      <div class="flex flex-col w-full col-span-3">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['wa']['hex']"
-          v-on:change="hexToHsl('wa')"
-        >
-        <div class="text-xs ">
-          warning
-        </div>
-      </div>
-      <div class="flex flex-col w-full col-span-3">
-        <input
-          type="color"
-          class="block w-full h-14"
-          v-model="colorValues['er']['hex']"
-          v-on:change="hexToHsl('er')"
-        >
-        <div class="text-xs ">
-          error
-        </div>
       </div>
     </div>
+  </div>
+</div>
 
-    <div class="prose">
-<pre><code>--d: {{ colorValues['d']['hsl'] }};
+
+<style>
+:root {
+--d: {{ colorValues['d']['hsl'] }};
 --p1: {{ colorValues['p1']['hsl'] }};
 --p2: {{ colorValues['p2']['hsl'] }};
 --p3: {{ colorValues['p3']['hsl'] }};
@@ -355,9 +120,9 @@
 --in: {{ colorValues['in']['hsl'] }};
 --su: {{ colorValues['su']['hsl'] }};
 --wa: {{ colorValues['wa']['hsl'] }};
---er: {{ colorValues['er']['hsl'] }};</code></pre>
-    </div>
-
+--er: {{ colorValues['er']['hsl'] }};
+}
+</style>
 
   </div>
 </template>
@@ -368,32 +133,32 @@ export default {
   data() {
     return {
       colorValues: {
-        "d"  : { hsl: "0 0% 100%", hex: "#ffffff"},
-        "p1" : { hsl: "259 94% 61%", hex: "#793ef9"},
-        "p2" : { hsl: "259 94% 51%", hex: "#570df8"},
-        "p3" : { hsl: "259 94% 41%", hex: "#4506cb"},
-        "s1" : { hsl: "314 100% 57%", hex: "#ff24cc"},
-        "s2" : { hsl: "314 100% 47%", hex: "#f000b8"},
-        "s3" : { hsl: "314 100% 37%", hex: "#bd0091"},
-        "a1" : { hsl: "174 60% 61%", hex: "#60d7cb"},
-        "a2" : { hsl: "174 60% 51%", hex: "#37cdbe"},
-        "a3" : { hsl: "174 60% 41%", hex: "#2aa79b"},
-        "cp" : { hsl: "0 0% 100%", hex: "#ffffff"},
-        "cs" : { hsl: "0 0% 100%", hex: "#ffffff"},
-        "ca" : { hsl: "0 0% 100%", hex: "#ffffff"},
-        "c1" : { hsl: "220 14% 96%", hex: "#f3f4f6"},
-        "c2" : { hsl: "228 14% 93%", hex: "#ebecf0"},
-        "c3" : { hsl: "220 15% 84%", hex: "#d0d4dc"},
-        "c4" : { hsl: "218 14% 65%", hex: "#99a2b2"},
-        "c5" : { hsl: "220 14% 46%", hex: "#657086"},
-        "c6" : { hsl: "220 14% 37%", hex: "#515a6c"},
-        "c7" : { hsl: "219 14% 28%", hex: "#3d4451"},
-        "c8" : { hsl: "222 13% 19%", hex: "#2a2e37"},
-        "c9" : { hsl: "223 14% 10%", hex: "#16181d"},
-        "in" : { hsl: "207 90% 54%", hex: "#2094f3"},
-        "su" : { hsl: "174 100% 29%", hex: "#009485"},
-        "wa" : { hsl: "36 100% 50%", hex: "#ff9900"},
-        "er" : { hsl: "14 100% 57%", hex: "#ff5724"},
+        "d"  : { hsl: "0 0% 100%", hex: "#ffffff", name: "default"},
+        "p1" : { hsl: "259 94% 61%", hex: "#793ef9", name: "primary-lighten"},
+        "p2" : { hsl: "259 94% 51%", hex: "#570df8", name: "primary"},
+        "p3" : { hsl: "259 94% 41%", hex: "#4506cb", name: "primary-darken"},
+        "s1" : { hsl: "314 100% 57%", hex: "#ff24cc", name: "secondary-lighten"},
+        "s2" : { hsl: "314 100% 47%", hex: "#f000b8", name: "secondary"},
+        "s3" : { hsl: "314 100% 37%", hex: "#bd0091", name: "secondary-darken"},
+        "a1" : { hsl: "174 60% 61%", hex: "#60d7cb", name: "accent-lighten"},
+        "a2" : { hsl: "174 60% 51%", hex: "#37cdbe", name: "accent"},
+        "a3" : { hsl: "174 60% 41%", hex: "#2aa79b", name: "accent-darken"},
+        "cp" : { hsl: "0 0% 100%", hex: "#ffffff", name: "content-primary"},
+        "cs" : { hsl: "0 0% 100%", hex: "#ffffff", name: "content-secondary"},
+        "ca" : { hsl: "0 0% 100%", hex: "#ffffff", name: "content-accent"},
+        "c1" : { hsl: "220 14% 96%", hex: "#f3f4f6", name: "content-100"},
+        "c2" : { hsl: "228 14% 93%", hex: "#ebecf0", name: "content-200"},
+        "c3" : { hsl: "220 15% 84%", hex: "#d0d4dc", name: "content-300"},
+        "c4" : { hsl: "218 14% 65%", hex: "#99a2b2", name: "content-400"},
+        "c5" : { hsl: "220 14% 46%", hex: "#657086", name: "content-500"},
+        "c6" : { hsl: "220 14% 37%", hex: "#515a6c", name: "content-600"},
+        "c7" : { hsl: "219 14% 28%", hex: "#3d4451", name: "content-700"},
+        "c8" : { hsl: "222 13% 19%", hex: "#2a2e37", name: "content-800"},
+        "c9" : { hsl: "223 14% 10%", hex: "#16181d", name: "content-900"},
+        "in" : { hsl: "207 90% 54%", hex: "#2094f3", name: "info"},
+        "su" : { hsl: "174 100% 29%", hex: "#009485", name: "success"},
+        "wa" : { hsl: "36 100% 50%", hex: "#ff9900", name: "warning"},
+        "er" : { hsl: "14 100% 57%", hex: "#ff5724", name: "error"},
       },
       colors: {
         'variants': {
