4e82f56d9caebace75439e4f8c38ac34c1dcde31
Pouya <pouya.saadeghi@gmail.com>
2021-01-03 16:15:43 +0330

    change title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DaisyUI workshop  
+# DaisyUI Demo  
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/fb76b89b-0d38-40c5-b318-da866905d02c/deploy-status)](https://app.netlify.com/sites/daisyui/deploys)
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ function getFiles(path, format= 'css') {
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'DaisyUI workshop',
+    title: 'DaisyUI Demo',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
