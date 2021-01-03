d61511f46fee3454ce954fc5b586e517411b4c5a
saadeghi <saadeghi@users.noreply.github.com>
2021-01-03 12:53:17 +0000

    deploy: f07b16b9568b5d9072b2a4d53b511db57be715ae

---
+++ nuxt-files/static/1609678395/components/input-checkbox/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/components/input-checkbox", {data:[{}],fetch:[],mutations:void 0});
---
+++ nuxt-files/static/1609678395/components/input-toggle/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/components/input-toggle", {data:[{}],fetch:[],mutations:void 0});
