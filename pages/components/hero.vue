50dd00eae1116907a7913b1c544e7adc69dc4af6
Pouya <pouya.saadeghi@gmail.com>
2021-01-11 17:22:52 +0330

    changed hero section markup

--- pages/components/hero.vue
+++ pages/components/hero.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <Wrapper title="hero" classes="flex flex-col space-y-2">
-      <Hero class="h-screen bg-content-100">
+    <Wrapper title="hero">
+      <Hero class="min-h-screen bg-content-100">
         <div class="text-center hero-content">
-          <div class="hero-text center">
+          <div class="max-w-md">
             <h1 class="mb-5 text-5xl font-bold">
               Hello there
             </h1>
@@ -15,10 +15,11 @@
         </div>
       </Hero>
     </Wrapper>
-    <Wrapper title="hero with figure" classes="flex flex-col space-y-2">
-      <Hero class="h-screen bg-content-100">
-        <div class="hero-content">
-          <div class="hero-text">
+    <Wrapper title="hero with figure">
+      <Hero class="min-h-screen bg-content-100">
+        <div class="flex-col hero-content lg:flex-row-reverse">
+          <img class="max-w-sm rounded-lg shadow-2xl" src="https://picsum.photos/id/1005/600/600" />
+          <div>
             <h1 class="mb-5 text-5xl font-bold">
               Hello there
             </h1>
@@ -27,16 +28,13 @@
             </p>
             <Button class="btn-primary">Get Started</Button>
           </div>
-          <div class="hero-figure">
-            <img class="rounded-lg shadow-2xl" src="https://picsum.photos/id/1005/600/600" />
-          </div>
         </div>
       </Hero>
     </Wrapper>
-    <Wrapper title="hero with form" classes="flex flex-col space-y-2">
-      <Hero class="h-screen bg-content-100">
-        <div class="hero-content">
-          <div class="hero-text">
+    <Wrapper title="hero with form">
+      <Hero class="min-h-screen bg-content-100">
+        <div class="flex-col justify-center hero-content lg:flex-row">
+          <div class="text-center lg:text-left">
             <h1 class="mb-5 text-5xl font-bold">
               Hello there
             </h1>
@@ -44,37 +42,36 @@
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
           </div>
-          <div class="hero-figure">
-            <Card class="shadow-2xl bg-default">
-              <div class="card-body">
-                <FormControl>
-                  <label class="label">
-                    <span class="label-text">Email</span>
-                  </label>
-                  <Input class="input-text bordered" type="text" placeholder="email" />
-                </FormControl>
-                <FormControl>
-                  <label class="label">
-                    <span class="label-text">Password</span>
-                  </label>
-                  <Input class="input-text bordered" type="text" placeholder="password" />
-                  <label class="label">
-                    <a href="#" class="label-text-alt">Forgot password?</a>
-                  </label>
-                </FormControl>
-                <FormControl class="mt-6">
-                  <input class="btn btn-primary" type="button" value="Login">
-                </FormControl>
-              </div>
-            </Card>
-          </div>
+          <Card class="flex-shrink-0 w-full max-w-sm shadow-2xl bg-default">
+            <div class="card-body">
+              <FormControl>
+                <label class="label">
+                  <span class="label-text">Email</span>
+                </label>
+                <Input class="input input-bordered" type="text" placeholder="email" />
+              </FormControl>
+              <FormControl>
+                <label class="label">
+                  <span class="label-text">Password</span>
+                </label>
+                <Input class="input input-bordered" type="text" placeholder="password" />
+                <label class="label">
+                  <a href="#" class="label-text-alt">Forgot password?</a>
+                </label>
+              </FormControl>
+              <FormControl class="mt-6">
+                <input class="btn btn-primary" type="button" value="Login">
+              </FormControl>
+            </div>
+          </Card>
         </div>
       </Hero>
     </Wrapper>
-    <Wrapper title="hero" classes="flex flex-col space-y-2">
-      <Hero class="h-screen overlay" style="background-image: url(https://picsum.photos/id/1005/1600/1400)">
-        <div class="text-center hero-content">
-          <div class="hero-text center">
+    <Wrapper title="hero">
+      <Hero class="min-h-screen" style="background-image: url(https://picsum.photos/id/1005/1600/1400)">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="text-center hero-content text-content-100">
+          <div class="max-w-md">
             <h1 class="mb-5 text-5xl font-bold">
               Hello there
             </h1>
