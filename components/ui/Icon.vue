8666745bc00ba5c396dd16b24a4788bcd86a1da3
Pouya <pouya.saadeghi@gmail.com>
2021-01-12 18:46:15 +0330

    add icon

--- components/ui/Icon.vue
+++ components/ui/Icon.vue
@@ -15,6 +15,7 @@
     <path v-if="glyph === 'back'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
     <path v-if="glyph === 'search'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
     <path v-if="glyph === 'heart'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+    <path v-if="glyph === 'check'" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
   </svg>
 </template>
 
