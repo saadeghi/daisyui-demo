074f463707025996ed2e8438ff9ec6d34eff8f08
Pouya <pouya.saadeghi@gmail.com>
2021-01-15 12:59:37 +0330

    add window mockup

---
+++ pages/components/mockup-window.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Wrapper title="window mockup with border" classes="max-w-md">
+      <div class="border mockup-window border-content-200">
+        <div class="flex justify-center px-4 py-16 border-t border-content-200">
+          Hello!
+        </div>
+      </div>
+    </Wrapper>
+    <Wrapper title="window mockup with bg color" classes="max-w-md">
+      <div class="mockup-window bg-content-200">
+        <div class="flex justify-center px-4 py-16 bg-content-100">
+          Hello!
+        </div>
+      </div>
+    </Wrapper>
+  </div>
+</template>
