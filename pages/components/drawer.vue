1287e24ed38e500f4bc494e4b2bab360ede4c0e4
Pouya <pouya.saadeghi@gmail.com>
2021-01-20 21:13:58 +0330

    focusable label for drawer

--- pages/components/drawer.vue
+++ pages/components/drawer.vue
@@ -2,10 +2,10 @@
   <div>
 
     <Wrapper title="sliding drawer menu" classes="flex flex-col space-y-2">
-      <div class="overflow-hidden rounded-lg shadow artboard bg-content-100 drawer h-52">
+      <div class="overflow-hidden rounded-lg shadow bg-content-100 drawer h-52">
         <input id="my-drawer" type="checkbox" class="drawer-toggle"/>
         <div class="flex items-center justify-center drawer-content">
-          <label class="btn btn-primary" for="my-drawer">open menu</label>
+          <label class="btn btn-primary drawer-button" for="my-drawer">open menu</label>
         </div>
         <div class="drawer-side">
           <label class="drawer-overlay" for="my-drawer"></label>
