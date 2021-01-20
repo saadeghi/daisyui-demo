cf47c5d6c9f817b053988b05f5e8aabd9a4df620
Pouya <pouya.saadeghi@gmail.com>
2021-01-20 20:05:34 +0330

    add drawer component

---
+++ pages/components/drawer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <Wrapper title="sliding drawer menu" classes="flex flex-col space-y-2">
+      <div class="overflow-hidden rounded-lg shadow artboard bg-content-100 drawer h-52">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle"/>
+        <div class="flex items-center justify-center drawer-content">
+          <label class="btn btn-primary" for="my-drawer">open menu</label>
+        </div>
+        <div class="drawer-side">
+          <label class="drawer-overlay" for="my-drawer"></label>
+          <Menu class="max-w-xs p-4 bg-default text-content-700">
+            <MenuItem>
+              <a>Menu Item</a>
+            </MenuItem>
+            <MenuItem>
+              <a>Menu Item</a>
+            </MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </Wrapper>
+
+  </div>
+</template>
