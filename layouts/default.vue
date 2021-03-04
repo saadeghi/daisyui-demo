2f7be07a8c7a72d446b1a263a319f286a455bfba
Pouya <pouya.saadeghi@gmail.com>
2021-03-04 15:29:26 +0330

    fix theme change bug on firefox

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,12 +18,10 @@
         </div>
         <div class="flex-none">
           <label class="relative btn btn-square btn-ghost">
-            <span class="absolute inset-0 btn btn-square btn-ghost">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
-              </svg>
-            </span>
-            <select autofocus="true" class="opacity-0 btn btn-square" data-choose-theme>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
+            </svg>
+            <select autofocus="true" class="absolute inset-0 opacity-0 cursor-pointer" data-choose-theme>
               <option value="">theme: 🎨 auto</option>
               <option v-for="(theme, index) in themes" :value="theme.id">{{ theme.name }}</option>
             </select>
