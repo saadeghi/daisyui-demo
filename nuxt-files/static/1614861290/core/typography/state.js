bf4fd64c02059637c571ba17d291043207e5d6b4
saadeghi <saadeghi@users.noreply.github.com>
2021-03-04 12:40:29 +0000

    deploy: 8ae21618a845947703f47e23761cee2467ae42c6

--- nuxt-files/static/1614861290/core/typography/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1614861290",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcore\u002Ftypography",config:{app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
