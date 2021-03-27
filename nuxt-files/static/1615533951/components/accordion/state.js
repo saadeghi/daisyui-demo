d49cf604d2ca7d326a2779bf67cbcbbb07e716cf
saadeghi <saadeghi@users.noreply.github.com>
2021-03-27 08:55:16 +0000

    deploy: 65ce8a5037a6c504e38dcb4973d30086ba63ae73

--- nuxt-files/static/1615533951/components/accordion/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-files\u002Fstatic\u002F1615533951",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcomponents\u002Faccordion",config:{app:{basePath:"\u002F",assetsPath:"\u002Fnuxt-files\u002F",cdnURL:a}}}}(null));
