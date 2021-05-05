83bf375d6db586653e375b456e54c8b88bd326ab
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-05-05 23:23:01 +0430

    add dropdown in navbar example

--- pages/components/dropdown.vue
+++ pages/components/dropdown.vue
@@ -95,7 +95,7 @@
       </div>
     </Wrapper>
 
-    <Wrapper title="dropdown dropdown-open" classes="flex justify-center bg-base-200 py-6 rounded-box">
+    <Wrapper title="dropdown dropdown-open" classes="flex justify-center bg-base-200 h-72 py-6 rounded-box">
       <div class="dropdown dropdown-open">
         <button class="m-1 btn" tabindex="0">force open</button>
         <div class="w-64 shadow card dropdown-content bg-primary text-primary-content">
@@ -106,6 +106,36 @@
         </div>
       </div>
     </Wrapper>
+
+    <Wrapper title="dorpdown in navbar" classes="bg-base-200 h-72 p-6 rounded-box">
+
+        <Navbar class="mb-2 bg-base-300 rounded-box">
+          <div class="flex-1 px-2 lg:flex-none">
+            <a class="text-lg font-bold">
+              DaisyUI
+            </a>
+          </div>
+          <div class="justify-end flex-1 hidden px-2 lg:flex">
+            <div class="flex items-stretch">
+              <a class="btn btn-ghost rounded-btn">
+                Button
+              </a>
+              <div class="dropdown dropdown-end">
+                <button class="btn btn-ghost rounded-btn" tabindex="0">Dropdown</button>
+                <ul class="shadow menu dropdown-content bg-base-100 rounded-box w-52">
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 2</a></li>
+                  <li><a>Item 3</a></li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+        </Navbar>
+
+    </Wrapper>
+
+
 
   </div>
 </template>
